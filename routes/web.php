1c3cd1dafef539faa33b1deb22be12b62e504f1b
samuel <jesu99332@gmail.com>
2023-04-16 13:36:41 -0400

    Modificaciones

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RolController;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ParqueoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,4 +36,7 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles', RolController::class);
     Route::resource('usuarios', UsuarioController::class);
     Route::resource('blogs', BlogController::class);
+    
 });
+
+Route::resource('parqueos', ParqueoController::class);
